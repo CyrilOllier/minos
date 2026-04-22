a4b3ba5a1c2053ebaff4faecb70f587376d376aa
Cyril O <cyril_ollier@yahoo.fr>
2026-04-22 15:36:01 +0200

    Update main.ts

--- server-api/src/main.ts
+++ server-api/src/main.ts
@@ -3,6 +3,10 @@ import { createApp } from './app.js';
 const app = createApp();
 const port = Number(process.env.PORT ?? 3001);
 
+app.get('/', (_req, res) => {
+  res.send('Minos API OK');
+});
+
 app.listen(port, () => {
   console.log(`Minos API en écoute sur http://localhost:${port}`);
 });
